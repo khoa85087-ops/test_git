76bea78d2be1ddb14f485928db43b9772acb00ff
Nguyễn Anh Khoa <khoa85087@gmail.com>
2026-05-26 22:49:14 +0700

    auto update 2026-05-26 22:49:14

---
+++ GitAutoPush.ps1
@@ -0,0 +1,31 @@
+Start-Process powershell.exe -WindowStyle Hidden -ArgumentList '-ExecutionPolicy Bypass -Command "& {
+
+$repo = \"C:\Users\AD\Downloads\note_git_hub\"
+
+while ($true) {
+
+    try {
+
+        Set-Location $repo
+
+        $changes = git status --porcelain
+
+        if ($changes) {
+
+            git add .
+
+            $time = Get-Date -Format \"yyyy-MM-dd HH:mm:ss\"
+
+            git commit -m \"auto update $time\"
+
+            git push
+        }
+
+    }
+    catch {
+    }
+
+    Start-Sleep -Seconds 10
+}
+
+}"'
---
+++ start.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Users\AD\Downloads\note_git_hub\GitAutoPush.ps1"
