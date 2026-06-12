6e7ed53907aad785a90298d9945f24cd7ee453ab
Nguyễn Anh Khoa <khoa85087@gmail.com>
2026-06-12 22:31:31 +0700

    auto update 2026-06-12 22:31:31

--- eyecover.bat
+++ eyecover.bat
@@ -1,4 +1,1 @@
 powershell -ExecutionPolicy Bypass -File "C:\Users\AD\Downloads\f2z\EyeReminder.ps1"
-powershell @echo off
-powershell.exe -WindowStyle Hidden -Command "Start-Process powershell.exe -WindowStyle Hidden -ArgumentList '-ExecutionPolicy Bypass -File ""C:\Users\AD\Downloads\f2z\GitAutoPush.ps1""'"
-exit
