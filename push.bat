1c74c7dab575877ea2013b1b52a7e794a0b0b05c
Nguyễn Anh Khoa <khoa85087@gmail.com>
2026-06-12 23:01:33 +0700

    auto update 2026-06-12 23:01:33

---
+++ push.bat
@@ -0,0 +1,3 @@
+powershell @echo off
+powershell.exe -WindowStyle Hidden -Command "Start-Process powershell.exe -WindowStyle Hidden -ArgumentList '-ExecutionPolicy Bypass -File ""C:\Users\AD\Downloads\f2z\GitAutoPush.ps1""'"
+exit
